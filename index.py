beea2a95558e3e02644bd26d3811d4a036242f2c
Theo Alves Da Costa <theo.alvesdacosta@ekimetrics.com>
2023-10-02 21:34:55 +0200

    Updated app

--- index.py
+++ index.py
@@ -40,16 +40,18 @@ def load_image(path):
     return np.array(resized_image)
 
 
+lang = "EN" if language == "English" else "FR"
+
+
 @st.cache_data
-def load_data(language):
+def load_data(lang):
     data = pd.read_excel("assets/descriptions_engins.xlsx").head(27)
-    lang = "FR" if language == "Français" else "EN"
     mapping = data[[lang,"path_image"]].set_index(lang)["path_image"].to_dict()
     mapping = {k:load_image(v) for k,v in mapping.items()}
     return mapping,data
 
 
-mapping,data = load_data(language)
+mapping,data = load_data(lang)
 values = list(mapping.keys())
 
 combinations = list(itertools.combinations(values,2))
@@ -57,57 +59,70 @@ np.random.shuffle(combinations)
 
 if "index" not in st.session_state:
     st.session_state["index"] = 0
-    index = 0
-else:
-    index = st.session_state["index"]
+index = st.session_state["index"]
 
-with st.form(key = "my_form"):
+content = {
+    "FR":{
+        "title":"Quelle est votre perception de l'impact des différents engins de pêche sur les écosystèmes marins ?",
+        "content":"Pour chaque couple d'engins de pêche, cliquez sur celui que vous percevez comme ayant le plus d'impact négatif sur l'environnement.\n\nIl faut approximativement 20-25 minutes pour compléter le questionnaire, mais à tout moment, vous pouvez le quitter et y revenir plus tard. Vos réponses seront sauvegardées.",
+    },
+    "EN":{
+        "title":"What is your perception of the impact of different fishing gears on marine ecosystems?",
+        "content":"For each pair of fishing gears, click on the one you perceive as having the most negative impact on the environment.\n\nIt takes approximately 20-25 minutes to complete the questionnaire, but at any time, you can leave it and come back later. Your answers will be saved.",
+    },
+}
 
-    option1,option2 = combinations[index]
+st.write(f"### {content[lang]['title']}")
+st.info(content[lang]["content"])
 
-    title1,desc1 = option1.split(":",1)
-    title2,desc2 = option2.split(":",1)
-    title1 = title1.strip()
-    title2 = title2.strip()
-    desc1 = desc1.strip().capitalize()
-    desc2 = desc2.strip().capitalize()
-
-    image1 = mapping[option1]
-    image2 = mapping[option2]
-
-    # image1 = os.path.join(image_folder_path,mapping[option1])
-    # image2 = os.path.join(image_folder_path,mapping[option2])
-    # assert os.path.exists(image1), f"Image {image1} does not exist"
+# if "last_result" in st.session_state:
+#     st.write(st.session_state['last_result'])
 
 
-    col1,col2 = st.columns(2)
+combination = list(combinations[index])
+np.random.shuffle(combination)
+option1,option2 = combination
 
-    with col1:
-        st.image(image1,use_column_width=True)
-        st.markdown(f"#### {title1}\n{desc1}")
-    with col2:
-        st.image(image2,use_column_width=True)
-        st.markdown(f"#### {title2}\n{desc2}")
+title1,desc1 = option1.split(":",1)
+title2,desc2 = option2.split(":",1)
+title1 = title1.strip()
+title2 = title2.strip()
+desc1 = desc1.strip().capitalize()
+desc2 = desc2.strip().capitalize()
 
-    col3,col4 = st.columns(2)
-    
-    with col3:
-        submitted = st.form_submit_button("Option 1")
-        if submitted:
-            st.session_state["index"] += 1
-            index += 1
+image1 = mapping[option1]
+image2 = mapping[option2]
 
-    with col4:
-        submitted = st.form_submit_button("Option 2")
-        if submitted:
-            st.session_state["index"] += 1
-            index += 1
+# image1 = os.path.join(image_folder_path,mapping[option1])
+# image2 = os.path.join(image_folder_path,mapping[option2])
+# assert os.path.exists(image1), f"Image {image1} does not exist"
 
 
+col1,col2 = st.columns(2)
 
-    st.write(f"Progress: {index}/{len(combinations)}")
+with col1:
+    st.image(image1,use_column_width=True)
+    st.markdown(f"#### (1) {title1}\n{desc1}")
+with col2:
+    st.image(image2,use_column_width=True)
+    st.markdown(f"#### (2) {title2}\n{desc2}")
+
+col3,col4 = st.columns(2)
+
+message_button = "is more damaging" if lang == "EN" else "est plus destructeur" 
 
 
+def validate_option(result):
+    print(result)
+    st.session_state["last_result"] = result
+    st.session_state["index"] += 1
 
 
+with col3:
+    submitted1 = st.button(f"1 {message_button}",on_click=validate_option, args=({"winner":option1,"loser":option2,"n_trials":index},))
+
+with col4:
+    submitted2 = st.button(f"2 {message_button}",on_click=validate_option, args=({"winner":option2,"loser":option1,"n_trials":index},))
+
+st.write(f"Progress: {index}/{len(combinations)}")
 
